cca4e66a7485b1692cab66784dd573992ce0b473
Phil <p.clauzel90@gmail.com>
2015-07-21 20:47:12 +0100

    Add static pages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,33 @@
+class StaticPagesController < ApplicationController
+
+  def home
+  end
+
+  def damp_proofing
+  end
+
+  def rising_damp
+  end
+
+  def penetrating_damp
+  end
+
+  def rot
+  end
+
+  def flood_leak_damage
+  end
+
+  def basement_conversion
+  end
+
+  def monocouche_render
+  end
+
+  def woodworm_treatment
+  end
+
+  def plastering
+  end
+
+end
