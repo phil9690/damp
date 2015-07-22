82a07153ab1b5987f79cbd947f2663f83c8cd873
Phil <p.clauzel90@gmail.com>
2015-07-22 21:25:13 +0100

    Add contact route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get 'woodworm-treatment'	  => 'static_pages#woodworm_treatment'
   get 'plastering'		  => 'static_pages#plastering'
 
+  get 'contact'                   => 'static_pages#contact'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
